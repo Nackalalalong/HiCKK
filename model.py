1401d42ad46883abd591854e9de2af3957f0df62
nackalalalong <nak-online@hotmail.com>
2020-10-12 20:37:16 +0700

    fix model to v6

--- model.py
+++ model.py
@@ -7,68 +7,33 @@ class OurNet(nn.Module):
         super(OurNet, self).__init__()
         self.filter_numbers = [8,3,5,8,1]
         self.filter_sizes = [7,5,3,5,7]
-        self.padding = 12
+        self.padding = 6
         self.output_dim = 40 - self.padding
 
-        self.seq1 = nn.Sequential(
-            nn.Conv2d(1, 5, 3),
-            nn.ReLU(),
-            nn.BatchNorm2d(5),
-            nn.Dropout2d(0.3),
-            nn.Conv2d(5, 5, 3),
-            nn.ReLU(),
-            nn.BatchNorm2d(5),
-            nn.Dropout2d(0.3)
-        )
+        self.conv3 = nn.Conv2d(1, 8, 5, padding=2)
+        self.conv4 = nn.Conv2d(1, 8, 3, padding=1)
+        self.conv5 = nn.Conv2d(1, 8, 1)
 
-        self.seq2 = nn.Sequential(
-            nn.Conv2d(1,5,5),
-            nn.ReLU(),
-            nn.BatchNorm2d(5),
-            nn.Dropout2d(0.3),
-        )
+        self.conv = nn.Conv2d(8, 8, 3, padding=1)
+        self.dropout = nn.Dropout2d(0.3)
+        self.batchnorm = nn.BatchNorm2d(8)
 
-        self.conv = nn.Conv2d(5, 5, 3)
-
-        self.seq3 = nn.Sequential(
-            nn.Conv2d(1,5,7),
-            nn.ReLU(),
-            nn.BatchNorm2d(5),
-            nn.Dropout2d(0.3),
-        )
-
-        self.seq4 = nn.Sequential(
-            nn.Conv2d(1, 5, 9),
-            nn.ReLU(),
-            nn.BatchNorm2d(5),
-            nn.Dropout2d(0.3),
-        )
-        
         self.seqout = nn.Sequential(
-            nn.Conv2d(5, 1, 5),
+            nn.Conv2d(8, 1, 7),
             nn.ReLU(),
-            # nn.BatchNorm2d(8),
-            # nn.Dropout2d(0.3),
-            # nn.Conv2d(8, 4, 3),
-            # nn.ReLU(),
-            # nn.BatchNorm2d(4),
-            # nn.Dropout2d(0.3),
-            # nn.Conv2d(4, 1, 5),
-            # nn.ReLU(),
         )
-
 
     def forward(self, x):
-        x1 = self.seq1(x)
-        x2 = self.seq2(x)
-        x3 = self.seq3(x)
-        x4 = x1 + x2
-        x5 = self.conv(x4)
-        x6 = x5 + x3
-        x7 = self.conv(x6)
-        x9 = self.seq4(x)
-        x8 = x7 + x9
+        x3 = self.dropout(self.batchnorm(F.relu(self.conv3(x))))
+        x4 = self.dropout(self.batchnorm(F.relu(self.conv4(x))))
+        x5 = self.dropout(self.batchnorm(F.relu(self.conv5(x))))
 
-        out = self.seqout(x8)        
+        x10 = self.dropout(self.batchnorm(F.relu(self.conv(x3))))
+        x11 = x10 + x4
+
+        x12 = self.dropout(self.batchnorm(F.relu(self.conv(x11))))
+        x13 = x12 + x5
+
+        out = self.seqout(x13)        
 
         return out
